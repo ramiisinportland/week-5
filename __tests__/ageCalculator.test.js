771ab1d7b6a626e9b081a1623ec67897432bd7d3
Rami <ramiisinportland@gmail.com>
2021-04-16 11:51:45 -0700

    Add passing test

--- __tests__/ageCalculator.test.js
+++ __tests__/ageCalculator.test.js
@@ -4,6 +4,6 @@ import AgeCalculator from '../src/js/ageCalculator.js'
 describe('AgeCalculator', () => {
   test('Should creat a new class AgeCalculator', () => {
     let ageCalculator = new AgeCalculator(42);
-    expect(ageCalculator.presentYear).toEqual(40)
+    expect(ageCalculator.presentYear).toEqual(42);
   })
 })
