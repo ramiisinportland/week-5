605065520b5fbf850fa62d17c40cb48928092c54
Rami <ramiisinportland@gmail.com>
2021-04-16 12:50:23 -0700

    Add the 4th passing test

--- __tests__/ageCalculator.test.js
+++ __tests__/ageCalculator.test.js
@@ -15,7 +15,7 @@ describe('AgeCalculator', () => {
 
 test('Should test if venusYear is correct compaired to AgeCalculator', () => {
     let ageCalculator = new AgeCalculator(42);
-    expect(ageCalculator.venusYear).toEqual(40)
+    expect(ageCalculator.venusYear).toEqual(42);
   })
 
 
